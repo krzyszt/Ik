69670e1ad2f143b900ab9137cb529dd147b84f3c
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-07-15 10:22:49 +0100

    panel tools

--- app/view/main/MainTab.js
+++ app/view/main/MainTab.js
@@ -13,7 +13,97 @@ Ext.define('Ik.view.main.MainTab', {
                callback: function(panel, tool, e){
                   panel.removeAll();
                }
-            } 
+            },
+             { 
+               type: 'minimize',
+               tooltip: 'minimize'
+            },
+            { 
+               type: 'maximize',
+               tooltip: 'maximize'
+            },
+            { 
+               type: 'restore',
+               tooltip: 'restore'
+            },
+            { 
+               type: 'toggle',
+               tooltip: 'toggle'
+            },
+            { 
+               type: 'gear',
+               tooltip: 'gear'
+            },
+            { 
+               type: 'prev',
+               tooltip: 'prev'
+            },
+            { 
+               type: 'next',
+               tooltip: 'next'
+            },
+            { 
+               type: 'pin',
+               tooltip: 'pin'
+            },
+            { 
+               type: 'unpin',
+               tooltip: 'unpin'
+            },
+            { 
+               type: 'right',
+               tooltip: 'right'
+            },
+            { 
+               type: 'left',
+               tooltip:'left'
+            },
+            { 
+               type: 'down',
+               tooltip: 'down'
+            },
+            { 
+               type: 'up',
+               tooltip: 'up'
+            },
+            { 
+               type: 'refresh',
+               tooltip: 'refresh'
+            },
+            { 
+               type: 'plus',
+               tooltip: 'plus'
+            },
+            { 
+               type: 'minus',
+               tooltip: 'minus'
+            },
+            { 
+               type: 'search',
+               tooltip: 'search'
+            },
+            { 
+               type: 'save',
+               tooltip: 'save'
+            },
+            { 
+               type: 'help',
+               tooltip: 'help'
+            },
+            { 
+               type: 'print',
+               tooltip: 'print'
+            },
+            { 
+               type: 'expand',
+               tooltip: 'expand'
+            },
+            { 
+               type: 'collapse',
+               tooltip: 'collapse'
+            }
+            
+            
          ]
       });
 
